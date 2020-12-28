789f166fc023cdb6f33e7df80232967adf906a61
Japa96 <gu.borges83@gmail.com>
2020-12-28 17:02:31 -0300

    Inserindo pontuação e nível de dificuldade do Jogo.

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,13 +6,28 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 # Gerando um número inteiro de 0 a 99
-numero_secreto = random.randrange(100)
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+pontuacao = 1000
 
-total_de_tentativas = 3
+print("Qual o níve de dificuldade do jogo?\n")
+print("Fácil (1)")
+print("Médio (2)")
+print("Difícil (3)")
+
+nivel = int(input("\nInforme o nível do jogo: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    print("Sua pontuacação é de {}".format(pontuacao))
 
     # Usuário vai inputar o número que ele acredita que seja o número secreto!
     chute_str = input("Digite um número entre 1 e 100: ")
@@ -26,7 +41,7 @@ for rodada in range(1, total_de_tentativas + 1):
         menor = int(chute) < numero_secreto
 
         if (acertou):
-            print("\nVocê acertou o número secreto.")
+            print("\nVocê acertou o número secreto!.")
             break
         else:
             if(maior):
@@ -34,8 +49,14 @@ for rodada in range(1, total_de_tentativas + 1):
             elif(menor):
                 print("Você errou o número secreto. O seu chute foi menor que o número secreto.\n")
 
+            # Calculo de perda de pontos.
+            # Exemplo : Chute igual a 40 e o numero é 20, perde 20 pontos.
+            # Usar a função abs() para pegar sopmente o número independente do sinal.
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontuacao - pontos_perdidos
+
     else:
         print("\nDigite um número entre 1 e 100!\n")
         continue
 
-print("Fim do jogo! O número secreto é {}".format(numero_secreto))
+print("Fim do jogo! O número secreto é {}!".format(numero_secreto), "Sua pontuação final é de {}".format(pontos))

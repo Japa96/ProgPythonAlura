cf8260a52139bf8dcef85f3758bc9aa519e8966f
Japa96 <gu.borges83@gmail.com>
2021-01-04 23:12:44 -0300

    Iniciando o desenvolvimento do Joga e inserindo algumas tratativas, como remoção de espaços e comparação de letras.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,14 +4,22 @@ def jogar():
     print("Bem vindo ao jogo de Adivinhação!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "python"
 
     enforcou = False
     acertou = False
 
     # Enquanto não enforcou e não acertou, fica no loop game.
     while(not enforcou and not acertou):
-        print("Jogando...")
+
+        chute = input('Qual a letra? ')
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
 
     print("Fim do jogo")
 

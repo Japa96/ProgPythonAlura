7211f9122ebccaea0702856e0159425dda93cc96
Japa96 <gu.borges83@gmail.com>
2021-01-04 23:35:19 -0300

    Inserindo a impressão da quantidade de espaços para informar a quantidade de letras da palavra, de forma fixa e imprimindo caso o jogar acerte a mesma.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -5,21 +5,27 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "python"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     # Enquanto não enforcou e não acertou, fica no loop game.
     while(not enforcou and not acertou):
 
         chute = input('Qual a letra? ')
+        # A função strip vai remover qualquer espaço em branco.
         chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
             index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
